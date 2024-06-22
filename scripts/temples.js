ac3a5107f6add11fd7d6b5a58dccd7828957320b
ekworsham <145511698+ekworsham@users.noreply.github.com>
2024-06-22 18:44:15 -0400

    temple update

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,12 @@ const today = new Date();
 currentYear.textContent = today.getFullYear();
 
 lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+// code for hamburger icon
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
