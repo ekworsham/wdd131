34df0f95b2b572fa48d4fe367bbd7801a9797de7
ekworsham <145511698+ekworsham@users.noreply.github.com>
2024-06-30 14:51:45 -0400

    update

--- scripts/place.js
+++ scripts/place.js
@@ -6,3 +6,18 @@ const today = new Date();
 currentYear.textContent = today.getFullYear();
 
 lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+const temperature = 50;
+const wind = 20;
+
+function calculateWindChill(T,V) {
+    return 35.74 = (0.615*T) - (35.75*(Math.pow(V,0.15))) + (0.4275*T)*(Math.pow(V,0,16));
+}
+
+const tempElement = document.getElementById("temp");
+tempElement.innerHTML = temperature;
+
+document.querySelector("wind").innerText = wind;
+
+const chill = calculateWindChill(temperature, wind);
+document.getElementById("chill").innerText = chill.toFixed(2);
