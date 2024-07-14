f4858db9b050cf33edc21a0e2f34fb0cbdb72111
ekworsham <145511698+ekworsham@users.noreply.github.com>
2024-07-14 19:03:44 -0400

    update

--- week05/scripts/form.js
+++ week05/scripts/form.js
@@ -9,28 +9,28 @@ lastModified.textContent = `Last Modification: ${document.lastModified}`;
 
 const products = [
     {
-      "id": "fc-1888",
-      "name": "flux capacitor",
-      "avg-rating": 4.5
+        id: "fc-1888",
+        name: "flux capacitor",
+        averageRating: 4.5
     },
     {
-      "id": "fc-2050",
-      "name": "power laces",
-      "averagerating": 4.7
+        id: "fc-2050",
+        name: "power laces",
+        averageRating: 4.7
     },
     {
-      "id": "fs-1987",
-      'name': "time circuits",
-      "averagerating": 3.5
+        id: "fs-1987",
+        name: "time circuits",
+        averageRating: 3.5
     },
     {
-      "id": "ac-2000",
-      "name": "low voltage reactor",
-      "averagerating": 3.9
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averageRating: 3.9
     },
-    {
-      "id": "jj-1969",
-      "name": "warp equalizer",
-      "averagerating": 5.0
+    {   
+        id: "jj-1969",
+        name: "warp equalizer",
+        averageRating: 5.0
     }
   ];
