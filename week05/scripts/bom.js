ebf11e213b723473eae5691bf94d72cf45f7eb87
ekworsham <145511698+ekworsham@users.noreply.github.com>
2024-07-14 18:58:52 -0400

    wk05update

--- week05/scripts/bom.js
+++ week05/scripts/bom.js
@@ -1,22 +1,42 @@
 
-button.addEventListener('click', function() {
-    if (input.value.trim() !== '') {
-        const input = document.querySelector('#favchap');
-        const button = document.querySelector('button');
-        const list = document.querySelector('list'); 
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-        li.textContent = input.value;
-        deleteButton.textContent = 'X';
-        li.append(deleteButton);
-        list.append(li); 
-    };
+// button.addEventListener('click', function() {
+//     if (input.value.trim() !== '') {
+//         const input = document.querySelector('#favchap');
+//         const button = document.querySelector('button');
+//         const list = document.querySelector('list'); 
+//         const li = document.createElement('li');
+//         const deleteButton = document.createElement('button');
+//         li.textContent = input.value;
+//         deleteButton.textContent = 'X';
+//         li.append(deleteButton);
+//         list.append(li); 
+//     };
+// });
+
+button.addEventListener('click', () => {
+    if (input.value !== '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
 });
 
-deleteButton.addEventLlistener('click', function () {
-    list.removeChild(li);
-    input.focus();
-});
+function displayList(item) {
+    let li = document.createElement('li');
+    let deleteButton = document. createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+    li.append(deletebutton);
+    list.append(li);
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    });
+}
 
 input.value = '';
 input.focus();
@@ -25,6 +45,35 @@ const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
+    navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+deleteButton.addEventLlistener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
