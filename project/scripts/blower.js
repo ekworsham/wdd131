c27664ed0c7bcf35e1ce97e731cd6d60ec306352
ekworsham <145511698+ekworsham@users.noreply.github.com>
2024-07-21 19:04:23 -0400

    final update

--- project/scripts/blower.js
+++ project/scripts/blower.js
@@ -14,4 +14,57 @@ hamburgerElement.addEventListener('click', () => {
     hamburgerElement.classList.toggle('open');
 })
 
-//**********************      ******************//
+//**********************blowers******************//
+
+const blowers = [
+    {
+        blowerName: "Stihl BR 86 Handheld",
+        imageUrl: "images/handheld.jpg"
+    },
+    {
+        blowerName: "Stihl BR 700 Backpack",
+        imageUrl: "images/backpack.jpg"
+    },
+    {
+        blowerName: "Bill Goat F1302H Wheeled",
+        imageUrl: "images/pushblower.jpg"
+    }
+]
+
+const equipment = document.querySelector("#blowers");
+if (equipment) {
+    blowers.forEach(blower => {
+        const unit = document.createElement("div");
+        unit.setAttribute("class", "unit");
+        unit.innerHTML = `
+            <p>${blower.blowerName}</p>
+            <img src="${blower.imageUrl}" alt="blower" width="100" height="100" loading="lazy">
+        `;
+        equipment.appendChild(unit);
+    });
+}
+
+//**************complete training******************//
+function getCount() {
+    return localStorage.getItem("count")
+}
+
+function addCount() {
+    count++;
+    localStorage.setItem("count", count)
+}
+const completeTraining = document.querySelector("#completeTraining");
+
+let count = getCount();
+
+if (completeTraining) {
+    completeTraining.addEventListener("click", addCount());
+}
+
+//**************review html******************//
+const counter = document.querySelector ("#counter");
+
+if (counter) {
+    counter.innerHTML = getCount();
+}
+
